531ef22a893d2f2da1d5c0cbfe664263412d0bbc
msviniciius <msvinicius.ads@gmail.com>
2025-05-24 15:31:37 -0300

    feature: add create, update and delete

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -1,5 +1,62 @@
 class DirectoriesController < ApplicationController
+  before_action :set_directory, only: [:show, :edit, :update, :destroy]
+
   def index
     @directories = Directory.where(parent_id: nil).includes(:subdirectories, :file_entries)
+  end
+  
+  def show
+    @directories = @directory
+  end
+
+  def new
+    @directory = Directory.new(parent_id: params[:parent_id])
+  end
+
+  def create
+    @directory = Directory.new(directory_params)
+    if @directory.save
+      redirect_to directories_path, notice: 'Diretório criado com sucesso!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @directory = Directory.find(params[:id])
+    if @directory.update(directory_params)
+      redirect_to directories_path, notice: 'Diretório atualizado com sucesso!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    sub_count = @directory.subdirectories.count
+    if sub_count > 0
+      redirect_to directories_path, alert: 'Não é possível excluir um diretório que contém subdiretórios.'
+      render json: {
+        confirm: true,
+        message: 'Este diretório contém subdiretórios. Tem certeza que deseja excluí-lo?',
+      }
+    else
+      @directory.destroy
+      redirect_to directories_path, notice: 'Diretório excluído com sucesso!'
+    end
+  end
+
+  private
+
+  def set_directory
+    @directory = Directory.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to directories_path, alert: 'Diretório não encontrado.'
+  end
+
+  def directory_params
+    params.require(:directory).permit(:name, :parent_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :directories, only: [:index]
-  root "directories#index"
+  resources :directories do
+    resources :file_entries, only: [:index, :new, :create]
+  end
+
+  root 'directories#index'
 end
